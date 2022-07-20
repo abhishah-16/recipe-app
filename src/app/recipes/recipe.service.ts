f7c0b41811ae1ee308235ed0d11b62239112043b
abhishah <abhishah0961@gmail.com>
2022-07-20 11:06:48 +0530

    [error resolve] saveData method

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,22 +6,19 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
 
   recipeSelect = new EventEmitter<Recipe>()
-  // public recipes: Recipe[] = [
-  //   new Recipe('Tomato Soup', 'This is very nice soup.', 'https://walnuts.org/wp-content/uploads/2020/10/Cream-Tomato-Soup_cropped-900x600.jpg', [
-  //     new Ingredients('tomato', 20),
-  //     new Ingredients('chili', 20)
-  //   ]),
-  //   new Recipe('Idli', 'this is south indian recipe', 'https://www.thespruceeats.com/thmb/A_g1pdGtPZBJyJ9ycu18iDzegL4=/1887x1415/smart/filters:no_upscale()/idli-56a510b63df78cf772862c34.jpg', [
-  //     new Ingredients('chilli powder', 20)
-  //   ]),
-  //   new Recipe('Paneer Butter Masala', 'This is made from paneer', 'https://myfoodstory.com/wp-content/uploads/2021/07/restaurant-style-paneer-butter-masala-2.jpg', [
-  //     new Ingredients('paneer', 20)
-  //   ]),
-  //   new Recipe('Masala Dosa', 'This is south indian recipe', 'https://vismaifood.com/storage/app/uploads/public/8b4/19e/427/thumb__700_0_0_0_auto.jpg', [
-  //     new Ingredients('masala', 30)
-  //   ])
-  // ];
-  public recipes: Recipe[] = []
+  public recipes: Recipe[] = [
+    new Recipe('Tomato Soup', 'This is very nice soup.', 'https://walnuts.org/wp-content/uploads/2020/10/Cream-Tomato-Soup_cropped-900x600.jpg', [
+      new Ingredients('tomato', 20),
+      new Ingredients('chili', 20)
+    ]),
+    new Recipe('Idli', 'this is south indian recipe', 'https://i2.wp.com/kalimirchbysmita.com/wp-content/uploads/2017/10/Idli-03-1024x983.jpg?resize=1024%2C983 ', [
+      new Ingredients('chilli powder', 20)
+    ]),
+    new Recipe('Masala Dosa', 'This is south indian recipe', 'https://static.toiimg.com/thumb/54289752.cms?imgsize=495844&width=800&height=800', [
+      new Ingredients('masala', 30)
+    ])
+  ];
+  // public recipes: Recipe[] = []
   public n: number = this.recipes.length
   constructor(private slservice: ShoppingListService) { }
 
